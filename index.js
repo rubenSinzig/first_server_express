c5d05a8846ab54f94c88af324433a050185ce3ef
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-06 15:47:08 +0200

    req & res with json file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const { name } = require("ci-info");
 const express = require("express");
 const server = express();
+const userData = require("./user.json");
 
 const PORT = process.env.PORT;
 
@@ -19,8 +21,19 @@ server.get("/user/:id", (req, res) => {
   ];
 
   if (req.params.id > userArr.length) {
-    return res.send(`please write a number under ${userArr.length}`);
+    res.send(`please write a number under ${userArr.length}`);
   } else {
     res.send(`username : ${userArr[req.params.id]}`);
+  }
+});
+
+server.get("/profile/:id", (req, res) => {
+  if (req.params.id >= userData.length) {
+    res.send(`please write a number between 0 and ${userData.length - 1}`);
+  } else {
+    res.send(
+      `User : ${userData[req.params.id].name} 
+      ${userData[req.params.id].profileEmoji}`
+    );
   }
 });
